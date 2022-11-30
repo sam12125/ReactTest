c467556dfe3cb99307ccca853b2cf2e68dc50f45
Saiyam Vaid <saiyamvaid1997@gmail.com>
2022-11-30 16:17:23 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect } from 'react'
 
 function App() {
+
+  const [id,setId] = useState(0)
+  const [user,setUser] = useState([])
+
+  const fetchData = async () => {
+
+    const data = await fetch("https://reqres.in/api/users")
+
+    const response = await data.json()
+
+    console.log(response.data)
+
+    // console.log(response.data)
+  }
+
+  const handleSubmit = async(id) => {
+    console.log(id)
+
+    if(id>0 && id<=10){
+      const userData = await fetch(`https://reqres.in/api/users/${id}`)
+
+      const resp = await userData.json()
+  
+      setUser(resp.data)
+      console.log(user)
+      
+      // console.log(resp.data)
+    }else{
+      alert("Id is not valid")
+    }
+
+  }
+  
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Enter id" onChange={(e)=> setId(e.target.value)}/>
+      <button onClick={()=>{handleSubmit(id)}}>Submit</button>
+{/* 
+      {user.map(elem => {
+
+        <h1>{elem.first_name}</h1>
+
+      })} */}
+
     </div>
   );
 }
